1a982f7c130c821e7888e13ca35b858e578fa693
Manoja S S <manojass.in@gmail.com>
2025-11-23 23:54:57 +0530

    Add files via upload

---
+++ arithmeticalOperatorsDemonstration.java
@@ -0,0 +1,16 @@
+class arithmeticalOperatorsDemonstration {
+
+	public static void main(String[] args) {
+		var a = 10;
+		var b = 5;
+		var addResult = a + b;
+		var subResult = a - b;
+		var mulResult = a * b;
+		var divResult = a / b;
+		System.out.println("Sum: " + addResult);
+		System.out.println("Differenc: " + subResult);
+		System.out.println("Product: " + mulResult);
+		System.out.println("Quotient: " + divResult);
+	}
+
+}
---
+++ relationalOperatorsDemonstration.java
@@ -0,0 +1,13 @@
+class relationalOperatorsDemonstration {
+
+	public static void main(String[] args) {
+		System.out.println(5 < 10); // < less then Operators
+		System.out.println(10 > 5); // < greater then Operators
+		System.out.println(10 <= 5); // < less then or equal to Operators
+		System.out.println(5 >= 10); // < greater then or equal to Operators
+		System.out.println(5 == 5); // equal to Operators ( Checks Left value is Equal to right value )
+		System.out.println(10 != 5); // Not equal to Operators
+				
+	}
+
+}
